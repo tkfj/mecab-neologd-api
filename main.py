21cad2afe34dad062361bd376e26cb53b7fd2ca3
tkfj <tk@fjworks.jp>
2026-07-08 22:36:26 +0000

    バルク実行APIを実装

--- main.py
+++ main.py
@@ -46,9 +46,26 @@ except Exception as e:
 class ParseRequest(BaseModel):
     text: str
 
+class ParseRequests(BaseModel):
+    texts: List[str]
+
 @app.post("/parse")
-async def parse_text(
+async def parse_one(
     request: ParseRequest,
+    dic: str = Query("IPADic", description="使用する辞書: 'IPADic' または 'NEOlogd'")
+):
+    return {"analysis": await parse_impl([request.text], dic)[0]}
+
+
+@app.post("/parseAll")
+async def parse_all(
+    request: ParseRequests,
+    dic: str = Query("IPADic", description="使用する辞書: 'IPADic' または 'NEOlogd'")
+):
+    return {"analysis": await parse_impl(request.texts, dic)}
+
+async def parse_impl(
+    texts: List[str],
     dic: str = Query("IPADic", description="使用する辞書: 'IPADic' または 'NEOlogd'")
 ):
     if len(taggers)<=0:
@@ -57,22 +74,25 @@ async def parse_text(
     if tagger is None:
         raise HTTPException(status_code=422, detail="MeCab Tagger not found")
 
-    node = tagger.parseToNode(request.text)
     results = []
-    while node:
-        if node.surface:
-            f = node.feature.split(",")
-            results.append({
-                "surface": node.surface,
-                "pos": f[0] if len(f) > 0 else "",          # 品詞大分類 (例: 名詞)
-                "pos_detail1": f[1] if len(f) > 1 else "",  # 品詞詳細1 (例: 固有名詞)
-                "pos_detail2": f[2] if len(f) > 2 else "",  # 品詞詳細2 (例: 一般)
-                "pos_detail3": f[3] if len(f) > 3 else "",  # 品詞詳細3 (例: *)
-                "conjugated_type": f[4] if len(f) > 4 else "", # 活用型 (例: サ変・スル)
-                "conjugated_form": f[5] if len(f) > 5 else "", # 活用形 (例: 基本形)
-                "base_form": f[6] if len(f) > 6 else node.surface, # 原形 (例: 健康)
-                "reading": f[7] if len(f) > 7 else "",      # 読み (例: ケンコウ)
-                "pronunciation": f[8] if len(f) > 8 else "" # 発音 (例: ケンコー)
-            })
-        node = node.next
-    return {"analysis": results}
+    for text in texts:
+        node = tagger.parseToNode(text)
+        results_one = []
+        while node:
+            if node.surface:
+                f = node.feature.split(",")
+                results_one.append({
+                    "surface": node.surface,
+                    "pos": f[0] if len(f) > 0 else "",          # 品詞大分類 (例: 名詞)
+                    "pos_detail1": f[1] if len(f) > 1 else "",  # 品詞詳細1 (例: 固有名詞)
+                    "pos_detail2": f[2] if len(f) > 2 else "",  # 品詞詳細2 (例: 一般)
+                    "pos_detail3": f[3] if len(f) > 3 else "",  # 品詞詳細3 (例: *)
+                    "conjugated_type": f[4] if len(f) > 4 else "", # 活用型 (例: サ変・スル)
+                    "conjugated_form": f[5] if len(f) > 5 else "", # 活用形 (例: 基本形)
+                    "base_form": f[6] if len(f) > 6 else node.surface, # 原形 (例: 健康)
+                    "reading": f[7] if len(f) > 7 else "",      # 読み (例: ケンコウ)
+                    "pronunciation": f[8] if len(f) > 8 else "" # 発音 (例: ケンコー)
+                })
+            node = node.next
+        results.append(results_one)
+    return results
